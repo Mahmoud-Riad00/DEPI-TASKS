32bece2f687bfb40be0c93beb8c7c02db26d36da
mahmoud-Riad00 <faydmhmwd1@gmail.com>
2024-08-08 00:42:39 +0300

    ss

---
+++ testing/home.js
@@ -0,0 +1,88 @@
+// Window this for brouser window
+// document this for HTML document
+// console.log(1+1)
+// console.error('their is an error')
+// console.warn('warning')
+// window.alert('hey')
+// document.write('<h1>hellooo</h>')
+
+
+// var userdata=[ ]
+// console.log(userdata)
+// userdata.push(5)
+// console.log(userdata)
+// var userdata=[6]
+// console.log(userdata)
+// var x = null
+// function problemOne(x , y , z ){
+//     if(x > 0)
+//         {
+//         console.log(Math.max(x , y , z))
+//         console.log(Math.min(x , y , z))
+//     }
+//     else{
+//         console.log('negative')
+//     }
+// }
+// problemOne(7 , 5 , 6)
+// function problemone(x){
+//     if(x % 2 === 0){
+//         console.log('even')
+//     }else{
+//         console.log('odd')
+//     }
+// }
+// problemone(12)
+// var x , y , z = String
+// function problemone(x){
+//     if(x === ( 'a' || 'e' || 'i' || 'o' || 'u')){
+//         console.log('vowel')
+//     }else{
+//         console.log('odd')
+//     }
+// }
+// problemone("y") 
+
+//  function problemOne(x){
+//     for(var y = 1; y <= x ;y++ ){
+//         console.log(y)
+//     }
+
+//  }
+//  problemOne(5)
+
+// function problemOne(x) {
+//     if(x==1){
+//         console.log('31')
+//     }else if(x==2){
+//         console.log('28 & 29 in leap year')
+//     }else if(x==3){
+//         console.log('31')
+//     }else if(x==4){
+//         console.log('30')
+//     }else if(x==5){
+//         console.log('31')
+//     }else if(x==6){
+//         console.log('30')
+//     }else if(x==7){
+//         console.log('31')
+//     }else if(x==8){
+//         console.log('31')
+//     }else if(x==9){
+//         console.log('30')
+//     }else if(x==10){
+//       console.log('31')
+//     }else if(x==11){
+//         console.log('30')
+//     }else if(x==12){
+//         console.log('31')
+//     }
+// }
+// problemOne(2)  
+function problemOne(x , y){
+    for(var z=1 ; z <= y ; y = y + 1){
+        لانمكوطظ.
+ /        
+    }
+}
+problemOne(15)
