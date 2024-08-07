dda298869742c145639732a71fa230c314c2ed64
mahmoud-Riad00 <faydmhmwd1@gmail.com>
2024-08-08 02:33:01 +0300

    AA

--- tasks/js-assignment/js.js
+++ tasks/js-assignment/js.js
@@ -1,4 +1,110 @@
-function problemone(x){
-    console.log(x)
-}
-problemone(1)
+// function problemone(x){
+//     console.log(x)
+// }
+// problemone(1)
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// function problemtwo(x) {
+//     if(x%3==0 && x % 4==0){
+//         console.log('yes')
+//     }else{
+//         console.log('no')
+//     }
+// }
+// problemtwo(9)
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// function problemthree(x , y)
+// {
+//     console.log( Math.max ( x , y ))
+// }
+// problemthree(10,7)
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// function problemfour(x){
+//     if(x>0){
+//         console.log('possitive')
+//     }else{
+//         console.log('negative')
+//     }
+
+// }
+// problemfour(-1)
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// function problemfive(x ,y ,z){
+//     console.log( Math.max ( x , y ,z))
+//     console.log( Math.min ( x , y ))
+// }
+// problemfive(1,2,3)
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// function problemsix(x){
+//     if(x%2==0){
+//         console.log('even')
+//        }
+//     else{
+//         console.log('odd')
+//     } 
+//    }
+//    problemsix(1)
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+// var userdata= ['a','e','I','o','u'];
+// function proplemeight(x){
+//     if(userdata.includes(x) ){
+//         console.log('vowel')
+//     }else{
+//         console.log('')
+//     }
+// }
+// proplemeight('u')
+// ============================================================================================================================================
+
+
+//  function problemOne(x){
+//     for(var y = 1; y <= x ;y++ ){
+//         console.log(y)
+//     }
+
+//  }
+//  problemOne(5)
+
+// function problemOne(x) {
+//     if(x==1){
+//         console.log('31')
+//     }else if(x==2){
+//         console.log('28 & 29 in leap year')
+//     }else if(x==3){
+//         console.log('31')
+//     }else if(x==4){
+//         console.log('30')
+//     }else if(x==5){
+//         console.log('31')
+//     }else if(x==6){
+//         console.log('30')
+//     }else if(x==7){
+//         console.log('31')
+//     }else if(x==8){
+//         console.log('31')
+//     }else if(x==9){
+//         console.log('30')
+//     }else if(x==10){
+//       console.log('31')
+//     }else if(x==11){
+//         console.log('30')
+//     }else if(x==12){
+//         console.log('31')
+//     }
+// }
+// problemOne(2)  
+// function problemOne(x , y){
+    // for(var z=1 ; z <= y ; y = y + 1){
+
+// problemOne(15)
